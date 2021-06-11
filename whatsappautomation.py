9097f946fb93fbe8274926547d6d7a0aceb01874
Akash Rana <78015639+akashgujjar273@users.noreply.github.com>
2021-06-11 18:06:16 +0530

    Update whatsappautomation.py

--- whatsappautomation.py
+++ whatsappautomation.py
@@ -1,3 +1,3 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg("+917291097679", "this message is sen by whatsapp automation",13,18 )
+pywhatkit.sendwhatmsg("+phone number", "this message is sen by whatsapp automation",13,18 )
